727ddfadf72ecb28aeafd96801dba397ba36af79
João Guilherme <130799538+jgprado04@users.noreply.github.com>
2026-03-27 17:41:48 -0300

    feat: add mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { env } from "./core/configs/.env/env.validation";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    MongooseModule.forRoot(`${env.MONGODB_URI}/${env.MONGODB_DATABASE}`, {
+      connectionFactory: (connection) => {
+        connection.on("connected", () => {
+          console.info("MongoDB conectado", "Mongoose");
+        });
+        return connection;
+      },
     }),
   ],
   controllers: [],
